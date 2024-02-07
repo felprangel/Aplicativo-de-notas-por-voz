a53b5f6f71b6dbf34501478f9b93f8a65b356443
felpo <feliperangel897@gmail.com>
2024-02-07 20:27:23 -0300

    adicionado modal

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,3 +1,7 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+
 interface NoteCardProps {
   date: Date
   content: string
@@ -5,15 +9,32 @@ interface NoteCardProps {
 
 export function NoteCard(props: NoteCardProps) {
     return (
-        <button className='rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
+      <Dialog.Root>
+        <Dialog.Trigger className='rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
           <span className='text-sm font-medium text-slate-300'>
-            {props.date.toISOString()}
+            {formatDistanceToNow(props.date, { locale: ptBR, addSuffix: true })}
           </span>
           <p className='text-sm leading-6 text-slate-400'>
             {props.content}
           </p>
 
           <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
-        </button>
+        </Dialog.Trigger>
+
+        <Dialog.Portal>
+          <Dialog.Overlay className='inset-0 fixed bg-black/50'/>
+          <Dialog.Content className='fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none'>
+            <div className='flex flex-1 flex-col gap-3 p-5'>
+              <span className='text-sm font-medium text-slate-300'>
+                {formatDistanceToNow(props.date, { locale: ptBR, addSuffix: true })}
+              </span>
+
+              <p className='text-sm leading-6 text-slate-400'>
+                {props.content}
+              </p>
+            </div>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
     )
 }

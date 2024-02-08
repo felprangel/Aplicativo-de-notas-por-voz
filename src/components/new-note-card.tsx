52f6f3ceb4d696647d43fcb46208e0f47c34c3ed
felpo <feliperangel897@gmail.com>
2024-02-07 21:40:23 -0300

    adicionado notificação de sucesso no save da nota

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { X } from 'lucide-react'
 import { ChangeEvent, FormEvent, useState } from 'react'
+import { toast } from 'sonner'
 
 export function NewNoteCard() {
     const [shouldShowOnboarding, setShouldShowOnboarding] = useState(true)
@@ -20,6 +21,7 @@ export function NewNoteCard() {
 
     function handleSaveNote(event: FormEvent) {
         event.preventDefault()
+        toast.success('Nota criada com sucesso!')
     }
 
     return (
